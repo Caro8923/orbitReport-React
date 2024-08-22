af069bc1bbfce8a2d137d4f00c06a1f628eb0b42
Carolyn Smith <carolynsmith@slu.edu>
2024-08-21 21:40:06 -0500

    code working, all tests passing

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,14 @@ import Banner from "./components/Banner";
 import { useState } from "react";
 import satData from "./components/satData";
 
-
 function App() {
   const [sat, setSat] = useState(satData);
   const displaySats = [...new Set(satData.map((data) => data.orbitType))];
   const filterByType = (currentType) => {
-    const displaySats = satData.filter((newSatDisplay) => {
+      const displaySats = satData.filter((newSatDisplay) => {
       return newSatDisplay.orbitType === currentType;
     });
+    setSat(displaySats);
   }
   return (
     <div>
